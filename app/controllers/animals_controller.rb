02e349ede075bb5d52061647218227936db0218e
LEARN Academy <MichaelDReyes5@gmail.com>
2023-11-30 13:39:16 -0800

    finished story 2

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,48 +1,47 @@
 class AnimalsController < ApplicationController
+  # before_action :set_animal, only: [:show, :update, :destroy]
+
     def index
         @animals = Animal.all
         render json: @animals
     end
     
       def show
-        animal = Animal.find(params[:id])
-        render json: @animals
+        @sightings = @animal.sightings
+        render json: { animal: @animal, sightings: @sightings }
       end
     
       def create
-        @animal = Animal.new(animal_params)
+        animal = Animal.create(animal_params)
         if animal.valid?
-            render json: animal
+            render json: animal, status: :created
         else
-          render json: animal.errors
-        end
+          render json: animal.errors, status: :unprocessable_entity
       end
-    
+    end
+  
     
       def update
-        animal = Animal.find(params[:id])
-        animal.update(animal_params)
-        if animal.valid?
+       if animal.update(animal_params)
             render json: @animal
         else
-          render json: @animal.errors
+          render json: @animal.errors, status: :unprocessable_entity
         end
       end
     
       def destroy
-        animal = Animal.find(params[:id])
         if animal.destroy
             render json: @animal
         else 
-            render json: @animal
+            render json: @animal.errors, status: :unprocessable_entity
         end
       end
     
       private
     
-      def set_animal
-        @animal = Animal.find(params[:id])
-      end
+      # def set_animal
+      #   @animal = Animal.find(params[:id])
+      # end
     
       def animal_params
         params.require(:animal).permit(:common_name, :scientific_binomial)
